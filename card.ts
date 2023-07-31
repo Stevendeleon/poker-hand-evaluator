c431e9bca4e8bfb6230e1701ab77f7bae600895f
Steven DeLeon <me@stevendeleon.com>
2023-07-31 09:49:50 -0400

    JSDoc comments - card class

--- card.ts
+++ card.ts
@@ -1,3 +1,15 @@
+// Type definitions
+type RankMap = Map<string, number>;
+
+interface SuitMap {
+  [key: string]: number;
+}
+
+interface PrettySuitsMap {
+  [key: number]: string;
+}
+
+
 /**
  * Card class
  * @class Card
@@ -42,9 +54,15 @@ export class Card {
     this.card_int = arg as number;
   }
 
-  static from_string(string: string): Card {
-    const rankChar = string[0];
-    const suitChar = string[1];
+  /**
+   * Creates a new instance of the `Card` class from a string.
+   * @param {string} c A string representing a card.
+   * @returns A new instance of the `Card` class.
+   * @example const card = Card.from_string("As");
+   */
+  static from_string(c: string): Card {
+    const rankChar = c[0];
+    const suitChar = c[1];
     const rankInt: number | undefined = Card.CHAR_RANK_TO_INT_RANK.get(
       rankChar,
     );
@@ -60,39 +78,103 @@ export class Card {
     return Card.from_int(cardInt);
   }
 
-  static from_int(cardInt: number): Card {
-    return Object.assign(new Card(0), { card_int: cardInt });
+  /**
+   * Creates a new instance of the `Card` class from an integer.
+   * @param {number} c An integer representing a card.
+   * @returns A new instance of the `Card` class.
+   * @example const card = Card.from_int(0x200000);
+   */
+  static from_int(c: number): Card {
+    return Object.assign(new Card(0), { card_int: c });
   }
 
-  toString(): string {
+  /**
+   * Returns a string representation of the card.
+   * @returns A string representation of the card.
+   * @public
+   * @example const card = new Card("As");
+   * card.toString(); // "As"
+   */
+  public toString(): string {
     return Card.STR_RANKS[this.rank] + Card.INT_SUIT_TO_CHAR_SUIT[this.suit];
   }
 
-  repr(): string {
+  /**
+   * Returns a representation of the card.
+   * @returns A string representation of the card.
+   * @public
+   * @example const card = new Card("As");
+   * card.repr(); // "Card("As")"
+   */
+  public repr(): string {
     return `Card("${this.toString()}")`;
   }
 
   // Getters
+  // -------
+
+  /**
+   * Returns the rank of the card.
+   * @returns {number} The rank of the card.
+   * @public
+   * @example const card = new Card("As");
+   * card.rank; // 12
+   */
   get rank(): number {
     return (this.card_int >> 8) & 0xf;
   }
 
+  /**
+   * Returns the suit of the card.
+   * @returns {number} The suit of the card.
+   * @public
+   * @example const card = new Card("As");
+   * card.suit; // 1 (spades)
+   */
   get suit(): number {
     return (this.card_int >> 12) & 0xf;
   }
 
+  /**
+   * Returns the bitrank of the card.
+   * @returns {number} The bitrank of the card.
+   * @public
+   * @example const card = new Card("As");
+   * card.bitrank; // 0x200000
+   */
   get bitrank(): number {
     return (this.card_int >> 16) & 0x1fff;
   }
 
+  /**
+   * Returns the prime of the card.
+   * @returns {number} The prime of the card.
+   * @public
+   * @example const card = new Card("As");
+   * card.prime; // 41
+   */
   get prime(): number {
     return this.card_int & 0x3f;
   }
 
+  /**
+   * Returns the string representation of the card in a pretty format.
+   * @returns {string} The string representation of the card in a pretty format.
+   * @public
+   * @example const card = new Card("As");
+   * card.pretty_string; // "[ A ♠ ]"
+   */
   get pretty_string(): string {
     return `[ ${Card.STR_RANKS[this.rank]} ${Card.PRETTY_SUITS[this.suit]} ]`;
   }
 
+  /**
+   * Returns the binary string representation of the card.
+   * @returns {string} The binary string representation of the card.
+   * @public
+   * @example const card = new Card("As");
+   * card.binary_string; // "10000000000000000001011000001001001"
+   */
   get binary_string(): string {
     const bstr: string[] = this.card_int.toString(2).split("").reverse();
     const output: string[] = Array.from(
@@ -110,14 +192,40 @@ export class Card {
 }
 
 // Helper functions
+// ---------------
+
+/**
+ * Returns a list of cards from a list of card strings.
+ * @param {string[]} card_strs A list of card strings.
+ * @returns {Card[]} A list of cards.
+ * @public
+ * @example const cards = card_strings_to_int(["As", "2h", "Td", "Jc"]);
+ * cards; // [ Card("As"), Card("2h"), Card("Td"), Card("Jc") ]
+ */
 export function card_strings_to_int(card_strs: string[]): Card[] {
   return card_strs.map((card_str) => new Card(card_str));
 }
 
+/**
+ * Returns a prime product from a list of cards.
+ * @param {Card[]} cards A list of cards.
+ * @returns {number} A prime product.
+ * @public
+ * @example const cards = card_strings_to_int(["As", "2h", "Td", "Jc"]);
+ * prime_product_from_hand(cards); // 54694
+ */
 export function prime_product_from_hand(cards: Card[]): number {
   return cards.reduce((product, card) => product * card.prime, 1);
 }
 
+/**
+ * Returns a prime product from a rankbits.
+ * @param {number} rankbits A rankbits.
+ * @returns {number} A prime product.
+ * @public
+ * @example const rankbits = 0b0000000;
+ * prime_product_from_rankbits(rankbits); // 1
+ */
 export function prime_product_from_rankbits(rankbits: number): number {
   let product = 1;
 
@@ -130,18 +238,15 @@ export function prime_product_from_rankbits(rankbits: number): number {
   return product;
 }
 
+/**
+ * Returns a string of cards in pretty format from a list of cards.
+ * @param {Card[]} cards A list of cards.
+ * @returns {string} A string of cards in pretty format.
+ * @public
+ * @example const cards = card_strings_to_int(["As", "2h", "Td", "Jc"]);
+ * card_list_to_pretty_str(cards); // "[ A ♠ ] [ 2 ♥ ] [ T ♦ ] [ J ♣ ]"
+ */
 export function card_list_to_pretty_str(cards: Card[]): string {
   return cards.map((card) => card.pretty_string).join(" ");
 }
 
-// Type definitions
-
-type RankMap = Map<string, number>;
-
-interface SuitMap {
-  [key: string]: number;
-}
-
-interface PrettySuitsMap {
-  [key: number]: string;
-}
